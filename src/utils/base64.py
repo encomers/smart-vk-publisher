ef198cebf3a0ab5a218219d271231d544490973c
neuron-nexus-agregator <gafarov.neuron.nexus@gmail.com>
2026-04-23 14:32:59 +0300

    docs

--- src/utils/base64.py
+++ src/utils/base64.py
@@ -4,6 +4,14 @@ import httpx
 
 
 async def to_base64_image(url: str) -> str:
+    """Скачивает изображение по URL и возвращает его в виде строки в формате data URI (base64).
+
+    Args:
+        url (str): Ссылка на изображение, которое нужно скачать и преобразовать.
+
+    Returns:
+        str: Валидная строка в формате data URI, содержащая изображение в виде base64.
+    """
     async with httpx.AsyncClient(timeout=20) as client:
         r = await client.get(url)
         r.raise_for_status()
