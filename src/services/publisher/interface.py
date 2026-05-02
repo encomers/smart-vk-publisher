f3c40f1ad2fa5571c8e1ad1b3f3ff32e01ad1b86
neuron-nexus-agregator <gafarov.neuron.nexus@gmail.com>
2026-05-02 16:39:02 +0300

    Added publisher interface

---
+++ src/services/publisher/interface.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+from src.model.domain.ready_text import ReadyText
+
+
+class IPublisher(ABC):
+    @abstractmethod
+    def publish(self, text: ReadyText) -> str:
+        """
+        Публикует статью с заданным заголовком, содержанием и изображением.
+        Возвращает URL опубликованной статьи.
+        """
+        pass
