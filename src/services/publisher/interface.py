57d9bbc28c48d5f163756b5a0754138eb72faf25
neuron-nexus-agregator <gafarov.neuron.nexus@gmail.com>
2026-05-02 17:07:35 +0300

    Config for VK Auth

--- src/services/publisher/interface.py
+++ src/services/publisher/interface.py
@@ -5,7 +5,7 @@ from src.model.domain.ready_text import ReadyText
 
 class IPublisher(ABC):
     @abstractmethod
-    def publish(self, text: ReadyText) -> str:
+    async def publish(self, text: ReadyText) -> str:
         """
         Публикует статью с заданным заголовком, содержанием и изображением.
         Возвращает идентификатор опубликованного поста.
